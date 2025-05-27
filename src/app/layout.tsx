25ed1dfccacfc8ea152e08d220627b3fa821960a
Sheru Shafiq <s.shafiq@reflex-systems.nl>
2025-05-27 16:37:38 +0200

    nextJS speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/app/ConvexClientProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <SpeedInsights/>
         <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
