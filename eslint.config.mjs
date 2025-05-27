b5f79b3fb4c2b238000f9ebc06ca98e7a874f58e
Sheru Shafiq <s.shafiq@reflex-systems.nl>
2025-05-27 16:55:28 +0200

    LINTTT

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import convexPlugin from "@convex-dev/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,11 +12,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+   ...convexPlugin.configs.recommended,
   {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: ["./tsconfig.json"],
-      tsconfigRootDir: __dirname
+    languageOptions: {
+      parser: await import("@typescript-eslint/parser"),
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname
+      }
     }
   },
   {
