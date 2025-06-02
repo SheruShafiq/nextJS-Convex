ceddd68a60f652c915c2fd2cc42e7b093eaaaa8c
Sheru Shafiq <s.shafiq@reflex-systems.nl>
2025-06-02 15:10:13 +0200

    test to see how well it works

--- src/app/familyGuySleepClient/page.tsx
+++ src/app/familyGuySleepClient/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+'use client';
+import React, { useState, useEffect } from "react";
 
 function Page() {
-  return <div>Page</div>;
+
+  const tmdb_id = "tt0182576"; // Replace with actual TMDB ID
+  const season_number = "1"; // Replace with actual season number
+  const episode_number = "1"; // Replace with actual episode number
+
+
+  return (
+    <div>
+      <h1>Family Guy Sleep Client</h1>
+      <p>Loading video...</p>
+      <iframe src={`https://vidsrc.xyz/embed/tv/${tmdb_id}/${season_number}-${episode_number}?autoplay=1&autonext=1`}></iframe>
+    </div>
+  );
 }
+
 
 export default Page;
